0f50474159b617b13eab6b952de03d5920db40d8
Edward John Steere <edward.steere@gmail.com>
2018-05-15 19:34:57 +0200

    Remove note about colliding missiles theyre not a thing

--- src/GameState.hs
+++ src/GameState.hs
@@ -12,6 +12,5 @@ update state (Command x' y' building') = state
 updateMissiles :: GameState -> [MissilePosition] -> GameState
 updateMissiles state missiles = foldr (flip updateMissile) state missiles
 
--- TODO: What about coliding missiles?
 updateMissile :: GameState -> MissilePosition -> GameState
 updateMissile state (MissilePosition missileToMove' newX' newY') = state
