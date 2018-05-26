cb516e2fa8f5d7f43fdf2338b2edd949e195898f
Edward John Steere <edward.steere@gmail.com>
2018-05-26 18:28:46 +0200

    Add stubs for building a building

--- src/GameState.hs
+++ src/GameState.hs
@@ -2,9 +2,26 @@ module GameState (update)
   where
 
 import Interpretor (GameState(..),
+                    CellContents(..),
+                    BuildingType(..),
                     Command(..))
+import GameMap
 
 -- TODO implement executing a command
 update :: GameState -> Command -> GameState
-update state NothingCommand            = state
-update state (Command x' y' building') = state
+update state NothingCommand                                                   = state
+update state@(GameState { gameMap = gameMap' }) (Command x' y' buildingType') =
+  case (getAt (x', y') gameMap') of
+    Nothing                          -> state
+    (Just (CellContents (Just _) _)) -> state
+    (Just (CellContents Nothing  _)) ->
+      state { gameMap = adjustAt (addBuilding buildingType')
+                                 (x', y')
+                                 gameMap' }
+
+todo = undefined
+
+addBuilding :: BuildingType -> CellContents -> CellContents
+addBuilding ATTACK  contents = todo
+addBuilding DEFENSE contents = todo
+addBuilding ENERGY  contents = todo
