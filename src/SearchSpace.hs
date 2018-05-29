b398ad97f924d0f22c7ec1f7e007a2bc394b66c0
Edward John Steere <edward.steere@gmail.com>
2018-05-29 07:17:39 +0200

    Implement functions to choose some of the better solutions

--- src/SearchSpace.hs
+++ src/SearchSpace.hs
@@ -8,6 +8,7 @@ module SearchSpace (myAvailableMoves,
 import Interpretor (GameState(..),
                     Command(..),
                     PlayerType(..))
+import System.Random
 import Engine
 import Cell
 import Logic
@@ -15,6 +16,7 @@ import GameState as G
 import Data.List as L
 import Player
 import Towers
+import Data.Maybe
 
 availableMoves :: ((Int, Int) -> Bool) -> (GameState -> Int) -> GameState -> [Command]
 availableMoves constrainCellsTo playerEnergy state@(GameState {gameMap = mapGrid}) = do
@@ -44,7 +46,34 @@ doNothingIfNoMoves xs = xs
 
 advanceState :: GameState -> [(GameState, Command)]
 advanceState state = do
-  let newState   = tickEngine state
+  let newState = tickEngine state
   myMove       <- doNothingIfNoMoves $ myAvailableMoves state
   oponentsMove <- doNothingIfNoMoves $ oponentsAvailableMoves state
   return $ (newState `updateMyMove` myMove `updateOponentsMove` oponentsMove, myMove)
+
+zipCDF :: [(GameState, Command)] -> [(Float, (GameState, Command))]
+zipCDF xs =
+  zip normalised $ sorted
+  where
+    normalised = map (/ (head summed)) summed
+    summed     = (reverse . scanl1 (+) . map (fromIntegral . boardScore . fst)) sorted
+    sorted     = sortOn (boardScore . fst) xs
+
+eliteChoices :: Int
+eliteChoices = 3
+
+chooseN :: RandomGen g => Int -> [(Float, (GameState, Command))] -> g -> ([(GameState, Command)], g)
+chooseN n xs g =
+  (elite ++ randomChoices, g''')
+  where
+    (_, max')              = genRange g
+    floatingMax            = fromIntegral max'
+    normalise              = (/ floatingMax) . fromIntegral . abs
+    elite                  = (map snd $ take eliteChoices xs)
+    (randomChoices, g''')  = foldr choose ([], g) [1..n]
+    choose _ (choices, g') =
+      let (value, g'') = next g'
+          normalised   = normalise value
+          scanForValue = (snd . fromJust . find ((<= normalised) . fst))
+      in (scanForValue xs : choices, g'')
+
