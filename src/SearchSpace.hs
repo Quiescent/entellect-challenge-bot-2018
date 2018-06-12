84441a9fe636875f4d68c8cac56b24fb771fceef
Edward John Steere <edward.steere@gmail.com>
2018-06-12 08:16:39 +0200

    Widen the search

--- src/SearchSpace.hs
+++ src/SearchSpace.hs
@@ -55,7 +55,7 @@ search g state =
     (initialChoices, g') = chooseN breadthToSearch g $ zipCDF $ map boardScore $ advanceState state
 
 breadthToSearch :: Int
-breadthToSearch = 2
+breadthToSearch = 20
 
 depthToSearch :: Int
 depthToSearch = 5
@@ -94,7 +94,7 @@ zipCDF xs =
     sorted     = sortOn fst xs
 
 eliteChoices :: Int
-eliteChoices = 1
+eliteChoices = 3
 
 chooseN :: RandomGen g => Int -> g -> [(Float, (GameState, Move))] -> ([(GameState, Move)], g)
 chooseN n g xs =
