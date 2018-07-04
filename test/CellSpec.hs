e17b3439b2ebf8ae520d9ba56d512438e1572a71
Edward John Steere <edward.steere@gmail.com>
2018-07-04 20:36:39 +0200

    Implement Cell tests

--- test/CellSpec.hs
+++ test/CellSpec.hs
@@ -6,22 +6,30 @@ import Cell
 
 spec :: Spec
 spec =
-  allCellsSpec             >>
   cellBelongsToMeSpec      >>
   cellBelongsToOponentSpec
-
-allCellsSpec :: Spec
-allCellsSpec = do
-  describe "allCells" $ do
-    it "should be implemented!" $ 0 `shouldBe` 1
 
 cellBelongsToMeSpec :: Spec
 cellBelongsToMeSpec = do
   describe "cellBelongsToMe" $ do
-    it "should be implemented!" $ 0 `shouldBe` 1
+    it "should produce true for a cell in the middle" $
+      cellBelongsToMe (7, 3) `shouldBe` True
+    it "should produce true for a cell on the extreme left" $
+      cellBelongsToMe (0, 3) `shouldBe` True
+    it "should produce false for a cell in the middle on the right" $
+      cellBelongsToMe (8, 5) `shouldBe` False
+    it "should produce false for a cell on the extreme right" $
+      cellBelongsToMe (15, 2) `shouldBe` False
 
 cellBelongsToOponentSpec :: Spec
 cellBelongsToOponentSpec = do
   describe "cellBelongsToOponent" $ do
-    it "should be implemented!" $ 0 `shouldBe` 1
+    it "should produce true for a cell in the middle" $
+      cellBelongsToOponent (7, 3) `shouldBe` False
+    it "should produce true for a cell on the extreoponent left" $
+      cellBelongsToOponent (0, 3) `shouldBe` False
+    it "should produce false for a cell in the middle on the right" $
+      cellBelongsToOponent (8, 5) `shouldBe` True
+    it "should produce false for a cell on the extreoponent right" $
+      cellBelongsToOponent (15, 2) `shouldBe` True
 
