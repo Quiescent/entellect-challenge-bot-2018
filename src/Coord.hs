f8713e05387b9395c82bda8989d18458c7d769c0
Edward John Steere <edward.steere@gmail.com>
2018-07-09 07:31:42 +0200

    Fix `fromCoord'

--- src/Coord.hs
+++ src/Coord.hs
@@ -11,4 +11,7 @@ toCoord :: Int -> Int -> Coord
 toCoord x y = y * width + x
 
 fromCoord :: Coord -> (Int, Int)
-fromCoord = (flip divMod) width 
+fromCoord = flipCoord . (flip divMod) width
+
+flipCoord :: (Int, Int) -> (Int, Int)
+flipCoord (x, y) = (y, x)
