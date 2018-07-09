a25bf0cd89abeff470b05d441419f587cb7f1a9c
Edward John Steere <edward.steere@gmail.com>
2018-07-09 07:31:33 +0200

    Fix cell ownership check

--- src/Cell.hs
+++ src/Cell.hs
@@ -10,10 +10,14 @@ allCells :: [Coord]
 allCells = [toCoord x y | x <- [0..width - 1], y <- [0..height - 1]]
 
 halfway :: Int
-halfway = toCoord (div width 2) 0
+halfway = div width 2
 
 cellBelongsToMe :: Coord -> Bool
-cellBelongsToMe = (< halfway)
+cellBelongsToMe coord =
+  let (x, _) = fromCoord coord
+  in (x < halfway)
 
 cellBelongsToOponent :: Coord -> Bool
-cellBelongsToOponent = (>= halfway)
+cellBelongsToOponent coord =
+  let (x, _) = fromCoord coord
+  in (x >= halfway)
