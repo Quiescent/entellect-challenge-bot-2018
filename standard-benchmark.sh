638cde569905fc31d72bf4ee80007d6a53c50283
Edward John Steere <edward.steere@gmail.com>
2018-07-11 21:09:07 +0200

    Add profiling flags to benchmark script

--- standard-benchmark.sh
+++ standard-benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/Test-Engine-exe Quiescent James /home/edward/wip/entelect-challenge-2018/starter-pack/tower-defence-matches/2018.07.09.21.14.50/
+./bin/Test-Engine-exe Quiescent James /home/edward/wip/entelect-challenge-2018/starter-pack/tower-defence-matches/2018.07.09.21.14.50/ +RTS -p -RTS
