376a460150d8b2a7d506082589b5d739c7ea2bb9
Edward John Steere <edward.steere@gmail.com>
2018-08-11 13:12:12 +0200

    Fix replay all matches script

--- replay-all.sh
+++ replay-all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-BASE_DIRECTORY=$1
-GAMES=$(ls $1/starter-pack/tower-defence-matches/)
+set -e
 
-for GAME in GAMES; do
-    PLAYER_1=$(ls "$GAME/Round 001" | sed -e "s/[AB] - //g" | head -1)
-    PLAYER_2=$(ls "$GAME/Round 001" | sed -e "s/[AB] - //g" | tail -1)
-    ./bin/TestEngine-exe $PLAYER_1 $PLAYER_2 $GAME
+BASE_DIRECTORY=$1
+GAMES=$(ls $1)
+
+for GAME in $GAMES; do
+    PLAYER_1=$(ls "$1/$GAME/Round 001" | sed -e "s/[AB] - //g" | head -1)
+    PLAYER_2=$(ls "$1/$GAME/Round 001" | sed -e "s/[AB] - //g" | tail -1)
+    ./bin/TestEngine-exe $PLAYER_1 $PLAYER_2 "$1/$GAME/"
 done
