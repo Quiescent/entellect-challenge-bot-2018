93c59898dbf9d04a6b9133b9168cf24109a80720
Edward John Steere <edward.steere@gmail.com>
2018-08-19 14:39:02 +0200

    Add missiles to the pattern to highlight for the log highlighter

--- highlight-console-replays.el
+++ highlight-console-replays.el
@@ -8,15 +8,15 @@
   "Highlight towers in replays."
   (interactive)
   (progn
-    (hi-lock-set-pattern "\\[[0-9]*e[0-9]*\\]" 'hi-blue-b)
-    (hi-lock-set-pattern "\\[[0-9]*E[0-9]*\\]" 'hi-blue-b)
-    (hi-lock-set-pattern "\\[[0-9]*a[0-9]*\\]" 'hi-red-b)
-    (hi-lock-set-pattern "\\[[0-9]*A[0-9]*\\]" 'hi-red-b)
-    (hi-lock-set-pattern "\\[[0-9]*d[0-9]*\\]" 'hi-green-b)
-    (hi-lock-set-pattern "\\[[0-9]*D[0-9]*\\]" 'hi-green-b)
-    (hi-lock-set-pattern "\\[[0-9]*t[0-9]*\\]" 'hi-pink)
-    (hi-lock-set-pattern "\\[[0-9]*T[0-9]*\\]" 'hi-pink)
-    (hi-lock-set-pattern "\\[[0-9]*[^eEaAdDtT][0-9]*\\]" 'hi-black-b)))
+    (hi-lock-set-pattern "\\[[0-9>< ]*e[0-9><]*\\]" 'hi-blue-b)
+    (hi-lock-set-pattern "\\[[0-9>< ]*E[0-9><]*\\]" 'hi-blue-b)
+    (hi-lock-set-pattern "\\[[0-9>< ]*a[0-9><]*\\]" 'hi-red-b)
+    (hi-lock-set-pattern "\\[[0-9>< ]*A[0-9><]*\\]" 'hi-red-b)
+    (hi-lock-set-pattern "\\[[0-9>< ]*d[0-9><]*\\]" 'hi-green-b)
+    (hi-lock-set-pattern "\\[[0-9>< ]*D[0-9><]*\\]" 'hi-green-b)
+    (hi-lock-set-pattern "\\[[0-9>< ]*t[0-9><]*\\]" 'hi-pink)
+    (hi-lock-set-pattern "\\[[0-9>< ]*T[0-9><]*\\]" 'hi-pink)
+    (hi-lock-set-pattern "\\[[0-9]*[>< ]+[0-9]*\\]" 'hi-black-b)))
 
 (provide 'highlight-console-replays)
 ;;; highlight-console-replays ends here
